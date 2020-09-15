1c2a7d611339fea3ab419544d83aa04f1543577c
김동휘 <wheeking@gmail.com>
2020-09-15 12:26:37 +0900

    Resolved S03P22A107-38 | Complete Read User Info and Chche Clear

--- musicD/src/main/java/ssafy/musicD/repository/UserRepoImpl.java
+++ musicD/src/main/java/ssafy/musicD/repository/UserRepoImpl.java
@@ -18,7 +18,7 @@ public class UserRepoImpl implements UserRepo {
 	public List<User> getUserInfo(String name) {
 		System.out.println("*********************");
 
-		Query query = new Query(new Criteria().andOperator(Criteria.where("name").is("whee")));
+		Query query = new Query(new Criteria().andOperator(Criteria.where("name").is(name)));
 		User user = new User();
 		user.setName("donghwi");
 		
