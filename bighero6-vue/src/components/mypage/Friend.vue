8cec9ddb481471ba38c33c5a53067402714358ae
윤지원 <linayune0320@naver.com>
2020-09-16 18:52:11 +0900

    S03P22A107-27 | Add friendSearch

--- bighero6-vue/src/components/mypage/Friend.vue
+++ bighero6-vue/src/components/mypage/Friend.vue
@@ -1,16 +1,86 @@
 <template>
 <div id ="friend">
   <div id = "friendList">
-      <h2>친구 목록</h2><br>
+      <div class="friendListTitle mt-0">
+        <h2 class="friendListText">친구 목록</h2><v-icon large  class="addUserBnt ml-6" @click="dialog=true">mdi-account-plus</v-icon>
+      </div>
+      <!-- 유저 검색 모달 창 -->
+      <v-dialog 
+        v-model="dialog"
+        max-width="600px"
+        primary
+      >
+        <v-card class="modal px-10">
+          <v-card-title>
+            추가하고 싶은 친구 닉네임으로 검색해주세요
+          </v-card-title>
+          <v-card-content>
+      <v-text-field
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        solo-inverted
+      ></v-text-field>
+      <v-virtual-scroll class="mt-4"
+        :items="items"
+        height="400"
+        item-height="64"
+      >
+        <template v-slot="{ item }">
+          <v-list-item >
+            <v-list-item-avatar>
+                <img :src="item.avatar">
+              </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>
+                 <strong>{{ item.title }}</strong>
+              </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn tile small color="primary">친구요청</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider></v-divider>
+        </template>
+      </v-virtual-scroll>
+
+     
+
+           
+          </v-card-content>
+          <v-card-actions>
+            <v-btn style="float:right;"
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+        
+
+      <v-text-field
+      class="friendSearch mx-auto mb-5"
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        solo-inverted
+      ></v-text-field>
 
     <v-card
-    
-      max-width="400"
+      max-width="600"
       class="mx-auto"
     >
       <v-virtual-scroll
         :items="items"
-        height="400"
+        height="500"
         item-height="64"
       >
         <template v-slot="{ item }">
@@ -36,8 +106,38 @@
     </v-card>
   </div>
   <div id="requestList">
-      <h2>요청 목록</h2>
-    
+      <h2 class="mt-7 mb-7">요청 목록</h2>
+       <v-card
+      max-width="600"
+      class="mx-auto"
+    >
+      <v-virtual-scroll
+        :items="items"
+        height="500"
+        item-height="64"
+      >
+        <template v-slot="{ item }">
+          <v-list-item >
+            <v-list-item-avatar>
+                <img :src="item.avatar">
+              </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>
+                 <strong>{{ item.title }}</strong>
+              </v-list-item-title>
+            </v-list-item-content>
+
+            <v-list-item-action class ="requestAccept">
+              <v-btn tile x-small color="primary" class="mr-2">수락</v-btn>
+              <v-btn tile x-small color="error">거절</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider></v-divider>
+        </template>
+      </v-virtual-scroll>
+    </v-card>
   </div>
 </div>
 </template>
@@ -93,11 +193,12 @@
           avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
           title: 'Recipe to try',
         }
-      ], 
+      ],
+      dialog : false
       }
     },
     methods: {
-   
+  
     },
     computed: {
        items () {
@@ -125,8 +226,23 @@
 }
   #friendList{
     float: left;
-}  
+}   
  #requestList{
     float: left;
 } 
+.friendSearch{
+  width:70%;
+}
+.friendListTitle{
+  display: inline-block;
+}
+.friendListText{
+  float: left;
+}
+.requestAccept{
+  display: inline;
+}
+.modal{
+  text-align: center;
+}
 </style>
