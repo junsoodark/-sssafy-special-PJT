65539536fe7119513b6abcbac068b6c003fe7218
Junsoo Park <junsoodark@naver.com>
2020-09-16 18:06:09 +0900

    Add signup page

--- bighero6-vue/src/components/account/Signup.vue
+++ bighero6-vue/src/components/account/Signup.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="login">
-    <v-container class="mx-auto" style="max-width:450px; margin-top:150px">
+    <v-container class="mx-auto" style="max-width:450px; margin-top:30px">
       <div class="inputForm mx-auto">
       <v-form>
+        <v-row align="center">
+            <v-col cols="100" sm="10">
+                <v-text-field
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Password"
+                    hint="At least 8 characters"
+                    counter
+                    @click:append="show1 = !show1"
+                ></v-text-field>
+            </v-col>
+        </v-row>
         <v-text-field v-model="email"
                       :rules="emailRules"
                       label="email"
                       required
         ></v-text-field>
+        <v-btn depressed small color = "primary">로그인</v-btn>
         <v-text-field
           v-model="password"
           :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
@@ -40,6 +56,7 @@
 export default {
   data() {
     return {
+         bottomNav: 'recent',
       password: 'Password',
       email: null,
       show1: false,
--- bighero6-vue/src/router/index.js
+++ bighero6-vue/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '../views/Home.vue'
 import MyPage from '../views/MyPage.vue'
 import Login from '../views/Login.vue'
+import Signup from '../views/SignupPage.vue'
 
 Vue.use(Router)
 
@@ -24,6 +25,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login,
-    }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+    },
   ],
 })
