f9da6e1f8d42bd41e60ba635a1be91372f9ecfcf
Junsoo Park <junsoodark@naver.com>
2020-09-17 19:13:38 +0900

    Modify signup page(front)

--- bighero6-vue/src/components/account/Signup.vue
+++ bighero6-vue/src/components/account/Signup.vue
@@ -1,36 +1,49 @@
 <template>
   <div id="login">
-    <v-container class="mx-auto" style="max-width:450px; margin-top:30px">
+    <v-container class="mx-auto" style="max-width:600px">
       <div class="inputForm mx-auto">
       <v-form>
-        <v-row align="center">
-            <v-col cols="100" sm="10">
-                <v-text-field
-                    v-model="password"
-                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                    :rules="[rules.required, rules.min]"
-                    :type="show1 ? 'text' : 'password'"
-                    name="input-10-1"
-                    label="Password"
-                    hint="At least 8 characters"
-                    counter
-                    @click:append="show1 = !show1"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-text-field v-model="email"
+        <v-row align="center" no-gutters>
+            <v-col cols="12" sm="9">
+                <v-text-field v-model="email"
                       :rules="emailRules"
                       label="email"
                       required
+                 ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="1">
+            </v-col>
+            <v-col cols="12" sm="2">
+              <v-btn depressed small color = "primary">인증코드 발송</v-btn>
+            </v-col>
+        </v-row>
+        <v-text-field v-model="certicode"
+              label="인증코드"
+              required
         ></v-text-field>
-        <v-btn depressed small color = "primary">로그인</v-btn>
+        <v-text-field v-model="nickname"
+                      :rules="[rules.nicknamerequired, rules.max]"
+                      label="nickname"
+                      required
+        ></v-text-field>
         <v-text-field
           v-model="password"
           :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :rules="[rules.required, rules.min]"
+          :rules="[rules.passwordrequired, rules.min]"
           :type="show1 ? 'text' : 'password'"
           name="input-10-1"
           label="Password"
+          hint="At least 8 characters"
+          counter
+          @click:append="show1 = !show1"
+        ></v-text-field>
+        <v-text-field
+          v-model="reenter"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="reenterRules"
+          :type="show1 ? 'text' : 'password'"
+          name="input-10-1"
+          label="Re-enter"
           hint="At least 8 characters"
           counter
           @click:append="show1 = !show1"
@@ -56,18 +69,25 @@
 export default {
   data() {
     return {
-         bottomNav: 'recent',
       password: 'Password',
-      email: null,
-      show1: false,
+      reenter: '',
+      certicode: '',
       email: '',
+      show1: false,
+      nickname: '',
       emailRules: [
         v => !!v || 'E-mail is required',
         v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
+      reenterRules: [
+        v => v== this.password || 'Password is not equals',
+        v => !!v || 'Re-enter is required',
+      ],
       rules: {
-        required: value => !!value || 'Password is Required.',
+        passwordrequired: value => !!value || 'Password is required.',
+        nicknamerequired: value => !!value || 'Nickname is required',
         min: v => v.length >= 8 || 'Min 8 characters',
+        max: v => v.length <= 10 || 'Max 10 characters',
       }
     };
   },
