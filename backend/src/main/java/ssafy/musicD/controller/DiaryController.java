409688129a8c758e83d20e21dbff3b45586b5a1f
Kwon <klw95@naver.com>
2020-09-30 15:55:56 +0900

    S03P23A107-14 | fix user update
    Former-commit-id: f03e51093db65f9fc87c2aa92eaabfedb00377fd

--- backend/src/main/java/ssafy/musicD/controller/DiaryController.java
+++ backend/src/main/java/ssafy/musicD/controller/DiaryController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 import ssafy.musicD.Domain.Diary;
-import ssafy.musicD.dto.StrDiary;
+import ssafy.musicD.dto.DiaryDto;
 import ssafy.musicD.service.DiaryService;
 
 @RestController
@@ -36,7 +36,7 @@ public class DiaryController {
 		System.out.println(userId);
 		System.out.println(month);
 		try {
-			List<StrDiary> diarys = diaryService.findDiaryByMonth(userId, month);
+			List<DiaryDto> diarys = diaryService.findDiaryByMonth(userId, month);
 			map.put("status", 200);
 			map.put("diarys", diarys);
 		} catch (Exception e) {
@@ -59,7 +59,7 @@ public class DiaryController {
 	// 일기 등록
 	@PostMapping
 	@ApiOperation(value = "일기 등록", response = String.class)
-	public Map<String, Object> registerDiary(@RequestBody StrDiary strDiary) {
+	public Map<String, Object> registerDiary(@RequestBody DiaryDto strDiary) {
 		Diary diary = new Diary();
 		diary.convertId(strDiary);
 		diaryService.insertDiary(diary);
@@ -71,7 +71,7 @@ public class DiaryController {
 	// 일기 수정
 	@PutMapping
 	@ApiOperation(value = "일기 수정", response = String.class)
-	public Map<String, Object> updateDiary(@RequestBody StrDiary strDiary) {
+	public Map<String, Object> updateDiary(@RequestBody DiaryDto strDiary) {
 		Diary diary = new Diary();
 		diary.convertId(strDiary);
 		diaryService.updateDiary(diary);
--- backend/src/main/java/ssafy/musicD/service/FriendServicelmpl.java
+++ backend/src/main/java/ssafy/musicD/service/FriendServicelmpl.java
@@ -2,7 +2,7 @@ package ssafy.musicD.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ssafy.musicD.dto.User;
+import ssafy.musicD.dto.MemberDto;
 import ssafy.musicD.repository.FriendRepolmpl;
 
 import java.util.List;
@@ -12,15 +12,15 @@ public class FriendServicelmpl implements FriendService {
     @Autowired
     private FriendRepolmpl friendRepo;
 
-    public List<User> searchFriends(String keyword , String userId){
+    public List<MemberDto> searchFriends(String keyword , String userId){
         return friendRepo.searchFriends(keyword,userId);
     }
 
-    public List<User> waitFriendList(String userId) { return friendRepo.waitFriendList(userId); }
+    public List<MemberDto> waitFriendList(String userId) { return friendRepo.waitFriendList(userId); }
 
-    public List<User> searchNonFriend(String keyword, String userId) { return friendRepo.searchNonFriend(keyword,userId);}
+    public List<MemberDto> searchNonFriend(String keyword, String userId) { return friendRepo.searchNonFriend(keyword,userId);}
 
-    public List<User> friendList(String userId) { return friendRepo.friendList(userId);}
+    public List<MemberDto> friendList(String userId) { return friendRepo.friendList(userId);}
 
     public void requestFriend(String userId, String friendId) {friendRepo.requestFriend(userId,friendId);}
 
