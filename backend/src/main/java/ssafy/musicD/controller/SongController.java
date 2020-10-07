4327ec8108d31024dd30687e2c39a9cd55c8d932
Kwon <klw95@naver.com>
2020-10-07 10:03:53 +0900

    fix songcontroller
    Former-commit-id: 1f1c6639e12d239c08729b774207ed8c43fa9514

--- backend/src/main/java/ssafy/musicD/controller/SongController.java
+++ backend/src/main/java/ssafy/musicD/controller/SongController.java
@@ -2,6 +2,7 @@ package ssafy.musicD.controller;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
