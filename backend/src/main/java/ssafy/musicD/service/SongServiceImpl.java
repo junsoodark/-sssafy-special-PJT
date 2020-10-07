80515c919af04a8375f7f4809b40c7613897387c
Kwon <klw95@naver.com>
2020-10-07 22:33:55 +0900

    fix searchSong, recommendSong
    Former-commit-id: 8881927011cb21c98f5e71d119296051d3d2126f

--- backend/src/main/java/ssafy/musicD/service/SongServiceImpl.java
+++ backend/src/main/java/ssafy/musicD/service/SongServiceImpl.java
@@ -5,17 +5,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ssafy.musicD.dto.Song;
 import ssafy.musicD.repository.SongRepo2;
+import ssafy.musicD.youtube.Youtube;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SongServiceImpl implements SongService{
     @Autowired
     private SongRepo2 songRepo;
+	@Autowired
+	private Youtube youtube;
 
-    public List<Song> searchSong(String keyword){return songRepo.searchSong(keyword); }
+
+    public List<Song> searchSong(String keyword){
+    	List<Song> songs = songRepo.searchSong(keyword);
+    	List<Song> result = new ArrayList();
+    	for(Song song: songs) {
+    		String search = song.getArtist()+" "+song.getSong_name();
+			String videoId = youtube.getVideoId(search);
+			song.setYoutubeId(videoId);
+			result.add(song);
+    	}
+    	return result;
+    }
     
     public List<Song> recommendSong(String emotion, String genre) {
-    	return songRepo.recommendSong(emotion, genre);
+    	List<Song> songs = songRepo.recommendSong(emotion, genre);
+    	List<Song> result = new ArrayList();
+    	for(Song song: songs) {
+    		String search = song.getArtist()+" "+song.getSong_name();
+			String videoId = youtube.getVideoId(search);
+			song.setYoutubeId(videoId);
+			result.add(song);
+    	}
+    	return result;
     }
 }
